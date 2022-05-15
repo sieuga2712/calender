e1a61067057a6b46679b6b06d878f0240e81f674
unknown <quangder123@gmail.com>
2022-05-15 22:46:01 +0700

    check time date personal

--- resources/views/MenuRight/createEvent.blade.php
+++ resources/views/MenuRight/createEvent.blade.php
@@ -88,46 +88,65 @@ $use=\App\Http\Controllers\Auth\loginController::userlogin();
 
 @endphp
 <form action="/createPersonalEvent" id="FormCreate" method="POST" style="margin-left:300px;margin-top:20px;">
-{{csrf_field()}}
+    {{csrf_field()}}
     <h2>nhap thong tin su kien</h2>
 
     <div>
         <label for="Eventname">ten su kien(*): </label>
         <input type="text" class="text" id="Eventname" name="Eventname">
         <br><br>
+        <label for="cycle" style="margin-left:20px;" disabled>su kien dai ngay</label>
+        <input type="checkbox" style="margin-left:20px;" id="cycle" onchange="checkck()"><br>
 
         <br><br>
-        <label for="startname">thoi gian bat dau: </label>
-        <input type="time" class="text" id="startname" name="startname" onchange="checktime()">
-        <br><br>
-        <label for="endname">thoi gian ket thuc:</label>
-        <input type="time" class="text" id="endname" name="endname" onchange="checktime()">
-        <br>
-        <label for="cycle" style="margin-left:20px;" disabled>su kien co chu ky</label>
-        <input type="checkbox" style="margin-left:20px;" id="cycle" onchange="checkck()" disabled><br>
 
-        <div id="chuky" style="visibility: hidden; display:none;">
-            <label for="datestart">ngay bat dau(*) </label>
-            <input type="date" class="text" id="datestart">
-            <br><br>
-            <label for="dateend">ngay ket thuc(*) </label>
-            <input type="date" class="text" id="dateend">
+
+        <div id="daingay" style="visibility: hidden; display:none;">
+            <label for="radio_chuky" style="margin-left:20px;" >su kien co chu ky</label>
+            <input type="radio" name="radio_chuky" style="margin-left:20px;" id="radio_chuky" onchange="check_chuky()" checked>
+            <br>
+            <label for="radio_khongchuky" style="margin-left:20px;" >su kien khong chu ky</label>
+            <input type="radio" name="radio_chuky" style="margin-left:20px;" id="radio_khongchuky" onchange="check_chuky()">
+            <br>
+            <div id="cochuky">
+                <label for="datestart">ngay bat dau(*) </label>
+                <input type="date" class="text" id="datestart" onchange="StartDayCheck(this.id)">
+                <br><br>
+
+                <label for="dateend">ngay ket thuc(*) </label>
+                <input type="date" class="text" id="dateend" onchange="EndDayCantBePast(this.id)">
+            </div>
+            <div id="khongchuky" style="display: none;">
+                <label for="kck_starttime">thoi gian bat dau: </label>
+                <input type="time" class="text" id="kck_starttime" name="kck_starttime" onchange="checktimekck(this.id)">
+                <br><br>
+                <label for="kck_endtime">thoi gian ket thuc:</label>
+                <input type="time" class="text" id="kck_endtime" name="kck_endtime"onchange="checktimekck(this.id)">
+                <br>
+                <label for="kck_date">ngay(*) </label>
+                <input type="date" class="text" id="kck_date" name="kck_date" onchange="EndDayCantBePast(this.id)">
+            </div>
+            <br>
+            <input type="button" onclick="writecode()" value="nhap">
+            <div id="write-code">
+
+            </div>
         </div>
 
         <div id="trongngay" style="visibility: visible;">
+            <label for="startname">thoi gian bat dau: </label>
+            <input type="time" class="text" id="startname" name="startname" onchange="checktimeinday(this.id)">
+            <br><br>
+            <label for="endname">thoi gian ket thuc:</label>
+            <input type="time" class="text" id="endname" name="endname" onchange="checktimeinday(this.id)">
+            <br>
             <label for="Eventdate">ngay(*) </label>
-            <input type="date" class="text" id="Eventdate" name="Eventdate">
+            <input type="date" class="text" id="Eventdate" name="Eventdate" onchange="EndDayCantBePast(this.id)">
 
         </div>
-        <br><br>
-        <label for="group">group: </label>
-        <select id="group" class="text" name="group" disabled>
-            <option value="0" style="color: red;">none</option>
-            <option value="hanoi">cntt02-k59</option>
+        <br>
 
-        </select>
-        <label for="checkgroup" style="margin-left:20px;">su kien nhom</label>
-        <input type="checkbox" style="margin-left:20px;" id="checkgroup" onchange="checkg()">
+
         <br><br>
         <label>ghi chu: <textarea id="note" class="text" name="note" rows="4" cols="38">
 
@@ -137,14 +156,14 @@ $use=\App\Http\Controllers\Auth\loginController::userlogin();
         @if ($che==1)
         <input type="submit" id="sudmit">
         @else
-        <input type="submit" id="sudmit"  disabled>
-        
+        <input type="submit" id="sudmit" disabled>
+
         @endif
-        
+
         <input type="button" onclick="lammoi()" value="lam moi">
         @if ($che!=1)
-       <div>ban chua dang nhap</div>
-        
+        <div>ban chua dang nhap</div>
+
         @endif
     </div>
 
@@ -154,15 +173,8 @@ $use=\App\Http\Controllers\Auth\loginController::userlogin();
 
 </form>
 <script>
-    function checkg() {
-        var g = document.getElementById("group");
-
-        g.disabled = !g.disabled;
-
-    }
-
     function checkck() {
-        var ck = document.getElementById("chuky");
+        var ck = document.getElementById("daingay");
         var n = document.getElementById("trongngay");
 
         if (ck.style.visibility.toString() == "hidden") {
@@ -184,9 +196,33 @@ $use=\App\Http\Controllers\Auth\loginController::userlogin();
         document.getElementById("FormCreate").reset();
 
     }
-    function checktime(){
-        var start=document.getElementById("startname");
-        var end=document.getElementById("endname");
-        
+    function check_chuky(){
+        var ck = document.getElementById("cochuky");
+        var n = document.getElementById("khongchuky");
+
+        if (ck.style.visibility.toString() == "hidden") {
+            ck.style.visibility = "visible";
+            n.style.visibility = "hidden";
+            ck.style.display = "inline-block";
+            n.style.display = "none";
+        } else {
+            ck.style.visibility = "hidden";
+            n.style.visibility = "visible";
+            ck.style.display = "none";
+            n.style.display = "inline-block";
+        }
+    }
+   
+
+    function writecode() {
+
+        var i = document.getElementById("Eventname");
+        var node = document.getElementById('write-code');
+        var newNode = document.createElement('p');
+        var start=document.getElementById("startname").value;
+        var end=document.getElementById("endname").value;
+        var date=document.getElementById("Eventdate").value;
+        newNode.appendChild(document.createTextNode(start+"-"+end+" "+date));
+        node.appendChild(newNode);
     }
 </script>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -65,10 +65,81 @@
 			elements[i].classList.add('hidden');
 
 	}
-	function EndDayCantPast(){
-		
+
+	function EndDayCantBePast(e) {
+		var end = document.getElementById(e);
+		var d = new Date().toISOString().slice(0, 10);
+		var today = new Date(d);
+		var endDay = new Date(end.value);
+		if (e != "dateend") {
+			if (today > endDay) {
+				alert("ngay ket thuc phai lon hon bang ngay " + today.toISOString().slice(0, 10));
+				end.value = today.toISOString().slice(0, 10);
+			}
+		} else {
+			var start = document.getElementById("datestart").value;
+
+			if (start == null) {
+				if (today > endDay) {
+					alert("ngay ket thuc phai lon hon bang ngay " + today.toISOString().slice(0, 10));
+					end.value = today.toISOString().slice(0, 10);
+				}
+			} else {
+				var st = new Date(start);
+
+				if (st > endDay) {
+					alert("ngay ket thuc phai lon hon bang ngay bat dau");
+					end.value = st.toISOString().slice(0, 10);
+				}
+			}
+		}
+
+
 	}
 
+	function StartDayCheck(e) {
+		var start = document.getElementById(e);
+
+		var startDay = new Date(start.value);
+		var end = document.getElementById("dateend").value;
+		var ed = new Date(end);
+		if (startDay > ed) {
+			alert("ngay ket thuc phai lon hon bang ngay bat dau");
+			start.value = ed.toISOString().slice(0, 10);
+		}
+
+	}
+
+	function checktimekck(e) {
+		if (e != "kck_endtime") {
+			var timestart = document.getElementById(e);
+			var timeend = document.getElementById("kck_endtime");
+		} else {
+			var timestart = document.getElementById("kck_starttime");
+			var timeend = document.getElementById(e);
+		}
+			
+
+		if (timeend.value < timestart.value && timestart.value!="" && timeend.value!="") {
+			alert("gio ket thuc phai hon gio bat dau");
+			timeend.value = timestart.value;
+		}
+	}
+	function checktimeinday(e) {
+		if (e == "startname") {
+			var timestart = document.getElementById(e);
+			var timeend = document.getElementById("endname");
+		} else {
+			var timestart = document.getElementById("startname");
+			var timeend = document.getElementById(e);
+		}
+			
+
+		if (timeend.value < timestart.value && timestart.value!="" && timeend.value!="") {
+			alert("gio ket thuc phai hon gio bat dau");
+			timeend.value = timestart.value;
+		}
+	}
 </script>
 <!--===============================================================================================-->
 <script src="js/mainTable.js"></script>
